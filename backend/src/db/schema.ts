d05d0eca05c0103584fc35b30ba40954a9c515be
iago <iago.scandido3@gmail.com>
2026-03-04 22:03:02 -0300

    initial commit

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,23 @@
+import {
+  boolean,
+  numeric,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const ticketsTable = pgTable("tickets", {
+  id: uuid().primaryKey().defaultRandom(),
+  description: varchar({ length: 255 }).notNull(),
+  client: varchar({ length: 255 }).notNull(),
+  address: varchar({ length: 255 }).notNull(),
+
+  value: numeric({ precision: 10, scale: 2 }).default("0").notNull(),
+  costs: numeric({ precision: 10, scale: 2 }).default("0").notNull(),
+
+  isCompleted: boolean("is_completed").default(false).notNull(),
+
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  completedAt: timestamp("completed_at"),
+});
---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import e from "express";
+
+const server = e();
+
+server.get("/", (_req, res) => {
+  res.status(200).json("Hello World");
+});
+
+server.listen(8080, "0.0.0.0", () => {
+  console.log("Server is running on port 8080");
+});
